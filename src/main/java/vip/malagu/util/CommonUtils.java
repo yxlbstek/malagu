9d5bd7b54aaf0b2cda31b16232ef2f378838e8c6
bobyangxiaolin <yxl17621020022@gmail.com>
2020-05-21 11:24:17 +0800

    CommonUtils

--- src/main/java/vip/malagu/util/CommonUtils.java
+++ src/main/java/vip/malagu/util/CommonUtils.java
@@ -1,17 +1,28 @@
 package vip.malagu.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import org.apache.poi.ss.formula.functions.T;
+import org.springframework.util.CollectionUtils;
 
-@SuppressWarnings("hiding")
-public final class TreeUtils {
+/**
+ * 工具类
+ * 1. 将 集合数据 转换成 树形结构数据
+ * 2. 将 树形结构数据 转换成 平铺数据
+ * 3. 收集 集合中 指定的 属性值
+ * 4. 集合source转Map，Key为source元素的propertyName属性值，Value为该元素
+ *
+ */
+public final class CommonUtils {
 
 	/**
-	 ** 将数据转换成 树形 结构
+	 * 将数据转换成 树形 结构
 	 * @param datas 需转换的数据集合
 	 * @param idProperty 集合中 对象的 主键 属性 (id)
 	 * @param parentIdProperty 集合中 对象的 父级ID 属性 (parentId)
@@ -49,7 +60,7 @@ public final class TreeUtils {
 	}
 	
 	/**
-	 * * 将 树形结构 数据转换成 平铺数据
+	 * 将 树形结构 数据转换成 平铺数据
 	 * @param datas 需要转换的数据集合
 	 * @param childrenProperty 集合中 对象的 子集集合 属性(children)
 	 * @return
@@ -69,6 +80,65 @@ public final class TreeUtils {
 				}
 			}
 		}
+	}
+
+	/**
+	 * 收集 集合中 指定的 属性值
+	 * @param source 数据集合
+	 * @param propertyName 属性名
+	 * @param <T> 范型
+	 * @return source集合每个对象的propertyName属性值的一个集合
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <T> Set<T> collect(Collection<?> source, String propertyName) {
+		if (CollectionUtils.isEmpty(source)) {
+			return Collections.EMPTY_SET;
+		}
+		Set result = new HashSet(source.size());
+		for (Object obj : source) {
+			Object value = null;
+			if (obj instanceof Map) {
+				value = ((Map) obj).get(propertyName);
+			} else if (obj != null) {
+				value = BeanUtils.getFieldValue(obj, propertyName);
+			}
+			if (value != null) {
+				result.add(value);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 集合source转Map，Key为source元素的propertyName属性值，Value为该元素
+	 * @param source 源集合
+	 * @param propertyName 属性名
+	 * @param <K> propertyName对应的属性的类型
+	 * @param <V> source集合元素类型
+	 * @return 索引Map
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <K, V> Map<K, V> index(Collection<V> source, String propertyName) {
+		if (CollectionUtils.isEmpty(source)) {
+			return Collections.EMPTY_MAP;
+		}
+		Map result = new HashMap();
+
+		for (Object obj : source) {
+			Object value = getValue(obj, propertyName);
+			result.put(value, obj);
+		}
+		return result;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static Object getValue(Object obj, String propertyName) {
+		if (obj instanceof Map) {
+			return ((Map) obj).get(propertyName);
+		} else if (obj != null) {
+			return BeanUtils.getFieldValue(obj, propertyName);
+		}
+		return null;
 	}
 
 }
